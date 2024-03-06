697b68eccdb7dc825fcf66c65e350beb9499b4be
Paulo Junior <npaulo.ce@gmail.com>
2024-03-06 13:27:35 -0300

    Aula TypeScript 05/03

--- fundamentos.ts
+++ fundamentos.ts
@@ -56,3 +56,101 @@ function validaOpcao(opcao1: string | number, opcao2: number | boolean) {
 
 validaOpcao('Paulo', true)
 validaOpcao(23, 24)
+
+// ALIAS
+type myAlias = number | string
+
+function showId(id: myAlias) {
+    console.log(`O ID é: ${id}`)
+}
+
+showId(15)
+showId('15')
+
+function showCpf(cpf: myAlias) {
+    console.log(`O CPF é: ${cpf}`)
+}
+
+showCpf(12345678910)
+showCpf('123.456.789.-10')
+
+type objectAlias = {
+    tipo: string,
+    valor: number
+}
+
+function userDetails(user: objectAlias) {
+    console.log(`Nome: ${user.tipo}`)
+    console.log(`Idade: ${user.valor}`)
+}
+
+userDetails({name: 'Paulo', idade: 26})
+
+// INTERFACES
+interface Coords {
+    x: number,
+    y: number,
+    z: number,
+}
+
+function showCoords(myObject: Coords) {
+    console.log(`x = ${myObject.x}, y = ${myObject.y} e z = ${ myObject.z } `)
+}
+
+let minhasCoordenadas = {
+    x: 10,
+    y: 15,
+    z: 28,
+}
+
+showCoords(minhasCoordenadas)
+
+
+// DIFERENÇA ENTRE ALIAS E INTERFACES
+// type myPet = {
+//     nome: string,
+//     peso: number,
+//     raca: string,
+//     idade: number,
+// }
+
+// function showPet (meuPet: myPet) {
+//     console.log(meuPet.nome)
+//     console.log(meuPet.raca)
+//     console.log(meuPet.idade)
+//     console.log(meuPet.peso)
+// }
+
+// showPet({nome: 'Floki', peso: 4, raca: 'Gatinho', idade: 32})
+
+// type myPet = {
+//     tipoRacao: string
+// }
+
+interface myPet {
+    nome: string,
+    peso: number,
+    raca: string,
+    idade: number,
+}
+
+function showPet (meuPet: myPet) {
+    console.log(meuPet.nome)
+    console.log(meuPet.raca)
+    console.log(meuPet.idade)
+    console.log(meuPet.peso)
+}
+
+const gatinhoDaAlice = {
+    nome: 'Floki', 
+    peso: 4, 
+    raca: 'Gatinho', 
+    idade: 32,
+    tipoRacao: 'Premier para gatos castrados'
+}
+
+showPet(gatinhoDaAlice)
+
+interface myPet {
+    tipoRacao: string
+}
